eacd38eaf1b8fa1810b544ebb4940492efc696be
jbartek87 <jbartek87@github.com>
2019-11-05 20:27:12 +0100

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,30 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        Calculator calculator = new Calculator();
+        if(calculator.add() == 15){
+            System.out.println("Test OK");
+        } else {
+            System.out.println("ERROR !");
+        }
+        if(calculator.substract() == 5) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    int a = 10;
+    int b = 5;
+
+    public int add(){
+        return a + b;
+    }
+
+    public int substract (){
+        return a - b;
+    }
+}
