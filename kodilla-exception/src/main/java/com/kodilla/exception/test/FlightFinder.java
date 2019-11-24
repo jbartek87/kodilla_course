6fdff342f62a5d22bf09aacc08965cdf80bb003e
jbartek87 <jbartek87@github.com>
2019-11-24 05:48:18 +0100

    Task 8.4

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -7,14 +7,15 @@ public class FlightFinder {
     Map<String, Boolean> myFlight = new HashMap<>();
 
     public boolean findFlight(Flight flight) throws RouteNotFoundException {
-        for (Map.Entry<String, Boolean> entry : myFlight.entrySet()) {
+
             if (myFlight.containsKey(flight.getDepartureAirport())) {
                 System.out.println("This flight is OK");
-                return entry.getValue();
+                return true;
             } else {
                 throw new RouteNotFoundException("Something went wrong");
+
             }
 
-        } return false;
+        }
+
     }
-}
