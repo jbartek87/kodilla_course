a86fbd16e4f580734d1bc9445c533e26d32b08ca
jbartek87 <jbartek87@github.com>
2019-11-24 11:54:51 +0100

    Task 8.4

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -3,18 +3,24 @@ package com.kodilla.exception.test;
 import java.util.Map;
 
 public class FlightFinder {
-    Map<String, Boolean> myFlight ;
+    private Map<String, Boolean> myFlight;
 
     public FlightFinder(Map<String, Boolean> myFlight) {
         this.myFlight = myFlight;
+        myFlight.put("Paris-London", true);
+        myFlight.put("Warsaw-London", true);
+        myFlight.put("Moscow-Dubai", true);
     }
 
     public boolean findFlight(Flight flight) throws RouteNotFoundException {
 
-        if (myFlight.containsKey(flight.getDepartureAirport())) {
-                System.out.println("This flight is OK");
-            return myFlight.get(flight.getDepartureAirport());
+        if (myFlight.containsKey(flight.getDepartureAirport() + "-" + flight.getArrivalAirport())) {
+            System.out.println("Flight from " + flight.getArrivalAirport() + " to "
+                    + flight.getDepartureAirport() + " is available");
+            return myFlight.get(flight.getDepartureAirport() + "-" + flight.getArrivalAirport());
             } else {
+            System.out.println("Flight from " + flight.getArrivalAirport() + " to "
+                    + flight.getDepartureAirport() + " is not available !!!");
                 throw new RouteNotFoundException("Something went wrong");
             }
 
--- kodilla-exception/src/main/java/com/kodilla/exception/test/ThirdChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ThirdChallenge.java
@@ -4,15 +4,11 @@ import java.util.HashMap;
 
 public class ThirdChallenge {
     public static void main(String[] args) {
-        Flight parisLondon = new Flight("Paris", "London");
+        Flight moscowParis = new Flight("Moscow", "Paris");
         FlightFinder flightFinder = new FlightFinder(new HashMap<>());
-        flightFinder.myFlight.put("Warsaw", true);
-        flightFinder.myFlight.put("Berlin", true);
-        flightFinder.myFlight.put("New York", true);
-        flightFinder.myFlight.put("Chicago", true);
 
         try{
-            flightFinder.findFlight(parisLondon);
+            flightFinder.findFlight(moscowParis);
         } catch (RouteNotFoundException e) {
             System.out.println("There is no such flight");
         }
