8696c237095fe3d5459c60e80f54f6a5212f276e
jbartek87 <jbartek87@github.com>
2020-01-14 21:02:45 +0100

    task 14.4

--- kodilla-spring-web/src/main/java/com/kodilla/kodilla/spring/web/KodillaSpringWebApplication.java
+++ kodilla-spring-web/src/main/java/com/kodilla/kodilla/spring/web/KodillaSpringWebApplication.java
@@ -8,6 +8,7 @@ public class KodillaSpringWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run( KodillaSpringWebApplication.class , args );
+        //
     }
 
 }
