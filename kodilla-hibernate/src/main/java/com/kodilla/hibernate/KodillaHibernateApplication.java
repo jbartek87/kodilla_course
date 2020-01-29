4b626c4d21ee05b1540bc9a4307a0384c38051bb
jbartek87 <jbartek87@github.com>
2020-01-29 05:57:16 +0100

    task 17.1

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KodillaHibernateApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(KodillaHibernateApplication.class, args);
     }
